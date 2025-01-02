e9bc718a1b34a628329930d511000adf9661e9ce
Francis <brakay1136@gmail.com>
2025-01-02 06:00:15 -0800

    fix large screen navbar

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const overlay = document.querySelector('.overlay')
 const mobileLinks = document.querySelector('.mobileLinks');
 const linkedList = document.querySelector('.linkedList');
 
+const links = document.querySelector('.links');
+
 // const arrowUp = document.querySelector('.arrow-up');
 // const arrowDown = document.querySelector('.arrow-down');
 
@@ -27,6 +29,28 @@ closeManu.addEventListener('click', () => {
 
 //Creation of event listener for dropdown manu of mobile navigation slider
 mobileLinks.addEventListener('click', (e) => {
+    
+    if (e.target.classList.contains('dropdown')) {
+        // Find the closest linked-list to the clicked button
+        const linkedList = e.target.nextElementSibling;
+       
+
+        //Check if the next elementSibling exist and has a classname of linkedList
+        if (linkedList && linkedList.classList.contains('linkedList')) {
+            linkedList.classList.toggle('dropdownLink');
+            
+
+            // Find the arrow within the clicked dropdown
+            const arrowDown = e.target.querySelector('.arrow-down');
+            if (arrowDown) {
+                arrowDown.classList.toggle('arrow-rotate');
+            }
+        
+        }
+
+    }
+})
+links.addEventListener('click', (e) => {
     
     if (e.target.classList.contains('dropdown')) {
         // Find the closest linked-list to the clicked button
